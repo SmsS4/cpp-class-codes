f32e63218ea8a326430b29cd77f449ab46b0959d
SmsS <smss.lite@gmail.com>
2022-02-24 09:54:55 +0330

    Add basic pointer

--- pointer.cpp
+++ pointer.cpp
@@ -3,4 +3,11 @@ using namespace std;
 
 
 int main() {
+    int x = 10;
+    cout << "Value x: " <<  x << '\n';
+    cout << "Address x: " << (&x) << '\n';
+    int* p2x = &x;
+    cout << "Address of pointed address: " << (p2x) << '\n';
+    cout << "Address of p2x: " << (&p2x) << '\n';
+    cout << "Value of p2x: " << (*p2x) << '\n';
 }
