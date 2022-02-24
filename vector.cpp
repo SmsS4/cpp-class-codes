581d57dc9b3f12085dda1d835320d2685db1c9d7
SmsS <smss.lite@gmail.com>
2022-02-24 11:35:06 +0330

    Add vector template

---
+++ vector.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Vector {
+    int size = 0;
+    int* a;
+    Vector() {
+        a = (int*) malloc(1*sizeof(int));
+    }
+
+    void expand() {
+        /// a ra 2 barar kon
+        /// copy
+    }
+
+    int get(int index) {
+        /// meghdar `index`-om ra bede
+        return -1;
+    }
+
+    void push_back(int x) {
+    }
+
+    void pop_back() {
+    }
+
+    int back() {
+        return -1;
+    }
+};
+
+int main() {
+    Vector v;
+}
