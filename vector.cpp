cd33b2a2051b31c00106eb60d6fc0206d56e1bb5
SmsS <smss.lite@gmail.com>
2022-02-24 12:32:02 +0330

    Add vector

--- vector.cpp
+++ vector.cpp
@@ -3,32 +3,59 @@ using namespace std;
 
 struct Vector {
     int size = 0;
+    int maxSize = 1;
     int* a;
     Vector() {
         a = (int*) malloc(1*sizeof(int));
     }
 
     void expand() {
-        /// a ra 2 barar kon
-        /// copy
+        int* b = (int*)malloc(maxSize*2*sizeof(int));
+        for (int i = 0; i < size; i++)
+            b[i] = a[i];
+        free(a);
+        a = b;
+        maxSize *= 2;
     }
 
     int get(int index) {
-        /// meghdar `index`-om ra bede
-        return -1;
+        return a[index];
     }
 
     void push_back(int x) {
+        a[size] = x;
+        size ++;
+        if (size == maxSize) {
+            expand();
+        }
     }
 
     void pop_back() {
+        size --;
     }
 
     int back() {
-        return -1;
+        return a[size-1];
     }
+
+
 };
 
 int main() {
     Vector v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    cout << v.size << '\n';
+    for (int i = 0; i < v.size; i++) {
+        cout << v.get(i) << ' ';
+    }
+    cout << '\n';
+    v.pop_back();
+    cout << v.size << '\n';
+    for (int i = 0; i < v.size; i++) {
+        cout << v.get(i) << ' ';
+    }
+
 }
+
