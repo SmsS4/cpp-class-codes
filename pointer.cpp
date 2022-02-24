09e6fe7826f094a3c94b98ca73caca59222d4bba
SmsS <smss.lite@gmail.com>
2022-02-24 09:57:11 +0330

    Add basic pointer

--- pointer.cpp
+++ pointer.cpp
@@ -9,5 +9,7 @@ int main() {
     int* p2x = &x;
     cout << "Address of pointed address: " << (p2x) << '\n';
     cout << "Address of p2x: " << (&p2x) << '\n';
-    cout << "Value of p2x: " << (*p2x) << '\n';
+    cout << "Value of p2x in ram: " << (*p2x) << '\n';
+    x = 15;
+    cout << (*p2x) << '\n'; // 15
 }
